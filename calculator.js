6acb8520f9129435e2fbe1210c9984c84f4be38b
Vinimello90 <160624296+Vinimello90@users.noreply.github.com>
2024-07-05 22:28:30 -0300

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,444 @@
+var value1 = 0;
+var value2 = 0;
+var preValue = 0;
+var operator = "";
+var currentValue = "num1";
+var displayCount;
+var prevInput = "";
+var prevCount;
+var result = "";
+var displayValue = "";
+var operatorSymbol = "";
+var negativeStatus = "";
+
+const element = document.getElementById("num");
+const calculatorBtn = document.querySelector(".calculator-btn");
+const inputContainer = document.querySelector(".input-container");
+
+calculatorBtn.addEventListener("click", openCalculator);
+inputContainer.addEventListener("click", closeCalculator);
+
+function click() {
+  const key = this.id;
+  const keyStyle = this.name;
+
+  buttonAnimation(keyStyle);
+  calculator(key);
+}
+
+function keyListener(event) {
+  event.target.blur();
+  const key = event.key;
+  let keyStyle = document.getElementById(key);
+  console.log(key);
+  keyStyle = keyStyle.name;
+  buttonAnimation(keyStyle);
+  calculator(key);
+}
+
+function openCalculator() {
+  const calculator = document.querySelector(".calculator");
+  if (calculator.classList.contains("display")) {
+    closeCalculator();
+    return;
+  }
+  calculator.classList.add("display");
+  const buttonNumbers = document.querySelectorAll(".btn-calc").length;
+  for (let i = 0; i < buttonNumbers; i++) {
+    document.querySelectorAll(".btn-calc")[i].addEventListener("click", click);
+  }
+  document.addEventListener("keydown", keyListener);
+}
+
+function closeCalculator() {
+  const calculator = document.querySelector(".calculator");
+  calculator.classList.remove("display");
+  const buttonNumbers = document.querySelectorAll(".btn-calc").length;
+  for (let i = 0; i < buttonNumbers; i++) {
+    document
+      .querySelectorAll(".btn-calc")
+      [i].removeEventListener("click", click);
+  }
+  document.removeEventListener("keydown", keyListener);
+}
+
+function buttonAnimation(keyButton) {
+  var activeButton = document.querySelector("." + keyButton);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 150);
+}
+
+function calculator(key) {
+  switch (key) {
+    case "Escape":
+      if (!value1) {
+        value2 = "";
+        element.innerHTML = 0;
+        preValue = 0;
+        prevInput = "";
+        currentValue = "num1";
+        operator = "";
+        document.getElementById("count").style.display = "none";
+        negativeStatus = false;
+        closeCalculator();
+        return;
+      }
+      value1 = "";
+      value2 = "";
+      preValue = 0;
+      operator = "";
+      currentValue = "num1";
+      displayCount = "";
+      prevInput = "";
+      prevCount = "";
+      result = "";
+      displayValue = "";
+      element.innerHTML = 0;
+      document.getElementById("count").style.display = "none";
+      element.style.fontSize = "38.5px";
+      negativeStatus = false;
+      break;
+    case "Backspace":
+      console.log("teste");
+      if (element.innerHTML.length === 1) {
+        element.innerHTML = "0";
+        prevInput = "";
+        currentValue === "num1" ? (value1 = "") : (value2 = "");
+        return;
+      }
+      var innnerHTML = element.innerHTML;
+      var slicedValue = innnerHTML.slice(0, innnerHTML.length - 1);
+      innnerHTML.length < 16 ? (element.style.fontSize = "21.7px") : null;
+      innnerHTML.length < 13 ? (element.style.fontSize = "27px") : null;
+      innnerHTML.length < 11 ? (element.style.fontSize = "38.5px") : null;
+      prevInput = slicedValue;
+      currentValue === "num1"
+        ? (value1 = parseFloat(slicedValue))
+        : (value2 = parseFloat(slicedValue));
+      element.innerHTML = slicedValue;
+      break;
+    case "0":
+      var input = 0;
+      getValues(input);
+      break;
+    case "1":
+      if (result) {
+        calculator("Escape");
+      }
+      var input = 1;
+      getValues(input);
+      break;
+    case "2":
+      if (result) {
+        calculator("Escape");
+      }
+      var input = 2;
+      getValues(input);
+      break;
+    case "3":
+      if (result) {
+        calculator("Escape");
+      }
+      var input = 3;
+      getValues(input);
+      break;
+    case "4":
+      if (result) {
+        calculator("Escape");
+      }
+      var input = 4;
+      getValues(input);
+      break;
+    case "5":
+      if (result) {
+        calculator("Escape");
+      }
+      var input = 5;
+      getValues(input);
+      break;
+    case "6":
+      if (result) {
+        calculator("Escape");
+      }
+      var input = 6;
+      getValues(input);
+      break;
+    case "7":
+      if (result) {
+        calculator("Escape");
+      }
+      var input = 7;
+      getValues(input);
+      break;
+    case "8":
+      if (result) {
+        calculator("Escape");
+      }
+      var input = 8;
+      getValues(input);
+      break;
+    case "9":
+      if (result) {
+        calculator("Escape");
+      }
+      var input = 9;
+      getValues(input);
+      break;
+    case ".":
+      if (result) {
+        calculator("Escape");
+      }
+      input = ".";
+      value2 === 0 ? (value2 = "") : null;
+      currentValue === "num1"
+        ? (value1 = value1.toString())
+        : (value2 = value2.toString());
+      const regex = /[.]/g;
+      let checkedNum = currentValue === "num1" ? value1 : value2;
+      const foundComa =
+        currentValue === "num1" ? value1.match(regex) : value2.match(regex);
+
+      if (!foundComa) {
+        getValues(input, currentValue);
+        if (checkedNum.length === 0 || parseInt(checkedNum.charAt(0)) === 0) {
+          element.innerHTML = "0.";
+          prevInput = element.innerHTML;
+          displayValue = element.innerHTML;
+          numComa === "num1"
+            ? (value1 = element.innerHTML)
+            : (value2 = element.innerHTML);
+          return;
+        }
+      }
+      if (foundComa) {
+        return;
+      }
+      break;
+    case "plusOrMinus":
+      const integer = parseFloat(element.innerHTML);
+      if (integer > 0) {
+        element.innerHTML = -Number(integer);
+        if (result) {
+          value1 = parseFloat(element.innerHTML);
+          return;
+        }
+        negativeStatus = true;
+      } else if (integer < 0) {
+        element.innerHTML = integer * -1;
+        if (result) {
+          value1 = parseFloat(element.innerHTML);
+          return;
+        }
+        negativeStatus = false;
+        return;
+      }
+
+      break;
+    case "%":
+      let percentValue = Math.abs((value2 / 100) * value1);
+      value2 = percentValue;
+      result = calculate(value1, value2, operator);
+      value1 = result;
+      result = result.toString();
+      displayResult(result);
+      preValue = percentValue;
+      value2 = "";
+      break;
+    case "/":
+      if (value2) {
+        result = calculate(value1, value2, operator);
+        value1 = result;
+        result = result.toString();
+        displayResult(result);
+        preValue = value2;
+      }
+      if (negativeStatus) {
+        currentValue === "num1"
+          ? (value1 = parseFloat(element.innerHTML))
+          : (value2 = parseFloat(element.innerHTML));
+      }
+      negativeStatus = false;
+      operator = "division";
+      operatorSymbol = "/";
+      displayCount = value1.toString();
+      count = `${displayCount.substring(0, 14)} /`;
+      document.getElementById("count").innerHTML = count;
+      document.getElementById("count").style.display = "block";
+      currentValue = "num2";
+      preValue = value2;
+      displayValue = "";
+      prevInput = "";
+      result = "";
+
+      break;
+    case "*":
+      if (value2) {
+        result = calculate(value1, value2, operator);
+        value1 = result;
+        result = result.toString();
+        displayResult(result);
+        preValue = value2;
+      }
+      if (negativeStatus) {
+        currentValue === "num1"
+          ? (value1 = parseFloat(element.innerHTML))
+          : (value2 = parseFloat(element.innerHTML));
+      }
+      negativeStatus = false;
+      operator = "multiply";
+      operatorSymbol = "*";
+      !value1 ? (value1 = 0) : null;
+      !value2 ? (value2 = 0) : null;
+      displayCount = value1.toString();
+      count = `${displayCount.substring(0, 14)} x`;
+      document.getElementById("count").innerHTML = count;
+      document.getElementById("count").style.display = "block";
+      currentValue = "num2";
+      displayValue = "";
+      prevInput = "";
+      result = "";
+      break;
+    case "-":
+      if (value2) {
+        result = calculate(value1, value2, operator);
+        value1 = result;
+        result = result.toString();
+        displayResult(result);
+        preValue = value2;
+      }
+      if (negativeStatus) {
+        currentValue === "num1"
+          ? (value1 = parseFloat(element.innerHTML))
+          : (value2 = parseFloat(element.innerHTML));
+      }
+      negativeStatus = false;
+      operator = "subtract";
+      operatorSymbol = "-";
+      displayCount = value1.toString();
+      count = `${displayCount.substring(0, 14)} -`;
+      document.getElementById("count").innerHTML = count;
+      document.getElementById("count").style.display = "block";
+      currentValue = "num2";
+      displayValue = "";
+      prevInput = "";
+      result = "";
+      break;
+    case "+":
+      if (value2) {
+        result = calculate(value1, value2, operator);
+        value1 = result;
+        result = result.toString();
+        displayResult(result);
+        preValue = value2;
+      }
+      if (negativeStatus) {
+        currentValue === "num1"
+          ? (value1 = parseFloat(element.innerHTML))
+          : (value2 = parseFloat(element.innerHTML));
+      }
+      negativeStatus = false;
+      operator = "addition";
+      operatorSymbol = "+";
+      displayCount = value1.toString();
+      count = `${displayCount.substring(0, 14)} +`;
+      document.getElementById("count").innerHTML = count;
+      document.getElementById("count").style.display = "block";
+      currentValue = "num2";
+      displayValue = "";
+      prevInput = "";
+      result = "";
+      break;
+    case "Enter":
+      if (negativeStatus) {
+        value2 = parseFloat(element.innerHTML);
+      }
+      if (!value1 && !value2 && !operator) {
+        return;
+      }
+      if (value1 && !operator) {
+        result = value1.toString();
+        displayResult(result);
+        return;
+      } else if (!value2) {
+        value2 = preValue;
+        result = calculate(value1, value2, operator);
+      }
+      result = calculate(value1, value2, operator);
+      result = Math.round(result * 100) / 100;
+      let showResult = result.toString();
+      displayCount = value2.toString();
+      displayResult(showResult);
+      preValue = value2;
+      value1 = result;
+      value2 = "";
+      negativeStatus = false;
+      break;
+  }
+}
+
+function getValues(input) {
+  console.log(prevInput > 0);
+  if ((displayValue.length >= 1 && displayValue === "0.") || prevInput > 0) {
+    let numValue = "" + prevInput + input;
+    numValue.length > 11 ? (element.style.fontSize = "33px") : null;
+    numValue.length > 13 ? (element.style.fontSize = "27px") : null;
+    numValue.length > 16 ? (element.style.fontSize = "21.7px") : null;
+    if (negativeStatus) {
+      element.innerHTML = "-" + numValue.substring(0, 20);
+    } else {
+      element.innerHTML = numValue.substring(0, 20);
+    }
+    prevInput = numValue;
+    currentValue === "num1"
+      ? (value1 = parseFloat(numValue))
+      : (value2 = parseFloat(numValue));
+
+    return;
+  }
+  currentValue === "num1" ? (value1 = input) : (value2 = input);
+  prevInput = input;
+  element.innerHTML = input;
+  displayValue = element.innerHTML;
+}
+
+function displayResult(resultValue) {
+  let showResult = resultValue;
+  showResult.length < 16 ? (element.style.fontSize = "21.7px") : null;
+  showResult.length < 13 ? (element.style.fontSize = "27px") : null;
+  showResult.length < 11 ? (element.style.fontSize = "38.5px") : null;
+  showResult.length > 11 ? (element.style.fontSize = "33px") : null;
+  showResult.length > 13 ? (element.style.fontSize = "27px") : null;
+  showResult.length > 16 ? (element.style.fontSize = "21.7px") : null;
+  element.innerHTML =
+    showResult === "NaN" ? "ERROR" : showResult.substring(0, 20);
+
+  if (showResult && !value2) {
+    document.getElementById("count").innerHTML = `${showResult.substring(
+      0,
+      14
+    )} =`;
+    document.getElementById("count").style.display = "block";
+    return;
+  }
+
+  let displayCount1 = value1.toString();
+  count = `${displayCount1.substring(0, 14)} ${operatorSymbol}`;
+  document.getElementById(
+    "count"
+  ).innerHTML = `${count} ${displayCount.substring(0, 14)} =`;
+}
+
+function calculate(num1, num2, operator) {
+  switch (operator) {
+    case "division":
+      return num1 / num2;
+    case "multiply":
+      return num1 * num2;
+    case "subtract":
+      return num1 - num2;
+    case "addition":
+      return num1 + num2;
+  }
+}
---
+++ index.js
@@ -0,0 +1,305 @@
+const inputForm = document.querySelectorAll("input");
+const addBtn = document.querySelector(".add-btn");
+const itemsList = document.querySelector(".items-list");
+const alert = document.querySelector(".alert");
+const clearBtn = document.querySelector(".clear");
+const monthBtnList = document.querySelector(".month-btns-list");
+const btn = document.querySelector(".input-container");
+
+const newDate = new Date();
+let editStatus = false;
+let editId = "";
+let editTitle = "";
+let editValue = "";
+const monthName = [
+  "janeiro",
+  "fevereiro",
+  "março",
+  "abril",
+  "maio",
+  "junho",
+  "julho",
+  "agosto",
+  "setembro",
+  "outubro",
+  "novembro",
+  "dezembro",
+];
+let monthInteger = newDate.getMonth();
+
+window.addEventListener("DOMContentLoaded", setupItems);
+window.addEventListener("click", clickOut);
+addBtn.addEventListener("click", addItemBtn);
+clearBtn.addEventListener("click", clearItems);
+
+function clickOut(e) {
+  if (e.target.id === "expensetitle") {
+    return;
+  }
+  if (e.target.id === "expensevalue") {
+    return;
+  }
+  if (e.target.className === "input-container show") {
+    return;
+  }
+  if (e.target.className !== addBtn.className) {
+    let btnCheck = btn.classList.contains("show");
+    if (btnCheck) {
+      btn.classList.remove("show");
+      console.log("teste");
+      addBtn.innerHTML = `<i class="fa-solid fa-plus"></i> Nova Despesa`;
+    }
+  }
+}
+
+function selectMonth(event) {
+  const monthBtns = document.querySelectorAll(".month-btn");
+  if (event.target) {
+    const element = event.target;
+    const monthId = event.target.dataset.id;
+    monthInteger = monthId;
+    const items = document.querySelectorAll(".item");
+    monthBtns.forEach((btn) => {
+      if (btn.classList.contains("btn-selected")) {
+        btn.classList.remove("btn-selected");
+      }
+    });
+    element.classList.add("btn-selected");
+    items.forEach((item) => {
+      item.remove();
+    });
+    setupItems();
+  } else {
+    monthBtns[2].classList.add("btn-selected");
+  }
+}
+
+function addItemBtn(e) {
+  e.preventDefault();
+  let btnCheck = btn.classList.contains("show");
+  if (!btnCheck) {
+    btn.classList.add("show");
+    addBtn.innerHTML = `<i class="fa-solid fa-plus"></i> Adicionar`;
+  } else {
+    const newDateId = new Date();
+    const id = newDateId.getTime().toString();
+    const title = expensetitle.value;
+    const value = expensevalue.value
+      ? parseFloat(expensevalue.value)
+      : expensevalue.value;
+    if (!title || !value) {
+    } else if (title && value && !editStatus) {
+      createItem(title, value, id);
+      setLocalStorage(title, value, id);
+      calculateTotal();
+      checkItems();
+      setDefault();
+      displayAlert("Item adicionado a lista!", "success");
+    } else if (title && value && editStatus) {
+      editTitle.innerHTML = title;
+      editValue.innerHTML = value.toLocaleString("pt-br", {
+        style: "currency",
+        currency: "BRL",
+      });
+      editLocalStorage(editId, title, value);
+      calculateTotal();
+      setDefault();
+      displayAlert("Item foi alterado!", "success");
+    }
+    btn.classList.remove("show");
+    addBtn.innerHTML = "Nova Despesa";
+  }
+}
+
+function createBtnsMonth(month) {
+  const element = document.createElement("li");
+  element.innerHTML = `<button type="button" class="month-btn" data-id="${month}">${monthName[month]}</button>`;
+  monthBtnList.appendChild(element);
+  const monthBtn = element.querySelector(".month-btn");
+  monthBtn.addEventListener("click", selectMonth);
+}
+
+function createItem(title, value, id) {
+  const element = document.createElement("div");
+  const attr = document.createAttribute("data-id");
+  const valueReal = value.toLocaleString("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  });
+  attr.value = id;
+  element.setAttributeNode(attr);
+  element.classList.add("item");
+  element.innerHTML = `<div class="item-content">
+    <p class="expense-title">${title}</p>
+    <p class="expense-value">${valueReal}</p>
+  </div>
+  <div class="container-item-btn">
+    <button type="button" class="edit item-btn">
+      <i class="fas fa-edit"></i>
+    </button>
+    <button type="button" class="delete item-btn">
+      <i class="fas fa-trash"></i>
+    </button>
+  </div>`;
+  itemsList.appendChild(element);
+  const editBtn = element.querySelector(".edit");
+  editBtn.addEventListener("click", editItem);
+  const deleteBtn = element.querySelector(".delete");
+  deleteBtn.addEventListener("click", deleteItem);
+}
+
+function editItem(e) {
+  e.stopPropagation();
+  const btn = document.querySelector(".input-container");
+  const element = e.currentTarget.parentElement.parentElement;
+  editId = element.dataset.id;
+  let items = getLocalStorage();
+  const selectedItem = items.filter(function (item) {
+    return item.id.includes(editId) ? item : null;
+  });
+  expensetitle.value = selectedItem[0].title;
+  expensevalue.value = selectedItem[0].value.toFixed(2);
+  addBtn.innerHTML = "Editar";
+  btn.classList.add("show");
+  editTitle = element.querySelector(".expense-title");
+  editValue = element.querySelector(".expense-value");
+  editStatus = true;
+}
+
+function deleteItem(e) {
+  const element = e.currentTarget.parentElement.parentElement;
+  const id = element.dataset.id;
+  element.remove();
+  removeFromLocalStorage(id);
+  calculateTotal();
+  checkItems();
+  setDefault();
+  displayAlert("Item removido da lista!", "danger");
+}
+
+function clearItems() {
+  const items = document.querySelectorAll(".item");
+  items.forEach((item) => {
+    item.remove();
+  });
+  localStorage.removeItem(monthName[monthInteger]);
+  calculateTotal();
+  checkItems();
+  setDefault();
+  displayAlert("Todos os items removidos da lista!", "danger");
+}
+
+function checkItems() {
+  const monthBtns = document.querySelectorAll(".month-btn");
+  const element = document.querySelector(".no-item-section");
+  const itemsList = document.querySelector(".items-list");
+  const noItem = document.querySelector(".no-item");
+
+  monthBtns[0].classList.add("btn-start");
+  monthBtns[2].classList.add("btn-end");
+
+  if (itemsList.children.length === 0) {
+    element.innerHTML = `            <div class="no-item">
+              <div class="no-item-content">
+                <p class="expense-title">Lista vazia, adicione novos itens!</p>`;
+  } else if (itemsList.children.length > 0 && noItem) {
+    noItem.remove();
+  }
+  itemsList.children.length > 0
+    ? clearBtn.classList.add("clear-show")
+    : clearBtn.classList.remove("clear-show");
+  const item = document.querySelectorAll(".item")[0];
+  itemsList.children.length > 0 ? item.classList.add("start") : null;
+}
+
+function setDefault() {
+  editStatus = false;
+  expensetitle.value = "";
+  expensevalue.value = "";
+  editId = "";
+  editTitle = "";
+  editValue = "";
+  addBtn.innerHTML = `<i class="fa-solid fa-plus"></i> Nova Despesa`;
+}
+
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+  setTimeout(() => {
+    alert.innerHTML = "";
+    alert.classList.remove(`alert-${action}`);
+  }, 1000);
+}
+
+function calculateTotal() {
+  const items = getLocalStorage();
+  const itemsValue = items.map((item) => {
+    let values = item.value;
+    return values;
+  });
+  let result = itemsValue.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
+  result = result.toLocaleString("pt-br", { minimumFractionDigits: 2 });
+  if (!result) {
+    document.querySelector(".real").innerHTML = "0";
+    document.querySelector(".decimal").innerHTML = "00";
+  }
+  const intNumber = result.slice(0, result.length - 3);
+  const decNumber = result.slice(result.length - 2, result.length);
+  document.querySelector(".real").innerHTML = intNumber;
+  document.querySelector(".decimal").innerHTML = decNumber;
+}
+
+function setupItems() {
+  const month = newDate.getMonth();
+  const months = [month - 2, month - 1, month];
+  if (document.querySelector(".month-btns-list").children.length === 0) {
+    months.map((month) => {
+      createBtnsMonth(month);
+    });
+    selectMonth(month);
+    localStorage.removeItem(monthName[monthInteger - 3]);
+  }
+  const title = document.querySelector(".title");
+  const year = newDate.getFullYear();
+  title.innerHTML = `Despesas ${monthName[monthInteger]} ${year}`;
+  const items = getLocalStorage();
+  items.map((item) => {
+    createItem(item.title, item.value, item.id);
+  });
+  itemsList.children.length > 0 ? checkItems() : null;
+  checkItems();
+  calculateTotal();
+}
+
+function setLocalStorage(title, value, id) {
+  const newItem = { title, value, id };
+  let items = getLocalStorage();
+  items.push(newItem);
+  localStorage.setItem(monthName[monthInteger], JSON.stringify(items));
+}
+
+function editLocalStorage(id, title, value) {
+  let items = getLocalStorage();
+  items = items.map((item) => {
+    return item.id === id ? { id: id, title: title, value: value } : item;
+  });
+  localStorage.setItem("expenses", JSON.stringify(items));
+}
+
+function removeFromLocalStorage(id) {
+  let items = getLocalStorage();
+  console.log(items);
+  items = items.filter((item) => {
+    return item.id !== id ? item : null;
+  });
+  console.log(items);
+  localStorage.setItem(monthName[monthInteger], JSON.stringify(items));
+}
+
+function getLocalStorage() {
+  return localStorage.getItem(monthName[monthInteger])
+    ? JSON.parse(localStorage.getItem(monthName[monthInteger]))
+    : [];
+}
